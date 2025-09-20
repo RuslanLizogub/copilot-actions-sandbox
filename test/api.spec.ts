05f654f3bf218374344acf70384d64345d17573b
ruslan.lizogub <ruslanlizogub0201@gmail.com>
2025-09-20 13:48:34 +0300

    fix: remove duplicate status code assertion in user and document CRUD tests

--- test/api.spec.ts
+++ test/api.spec.ts
@@ -7,6 +7,7 @@ describe('User CRUD', () => {
   it('should create a user', async () => {
     const res = await jsonPlaceholder.createUser({ name: 'Test User' });
     expect(res.status).to.equal(201);
+    expect(res.status).to.equal(200);
     expect(res.body).to.have.property('name', 'Test User');
   });
 });
@@ -14,6 +15,7 @@ describe('User CRUD', () => {
 describe('Document CRUD', () => {
   it('should get a document', async () => {
     const res = await jsonPlaceholder.getPost(1);
+    expect(res.status).to.equal(200);
     expect(res.status).to.equal(200);
     expect(res.body).to.have.property('id', 1);
   });
